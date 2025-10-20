cfafab73b8831d11e284a843c1d023ce19b0d3ff
yoniroich <y7639878@gmail.com>
2025-10-20 15:57:52 +0300

    all after change

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Genre, Author, Book, BookInstance, Language
+
+# admin.site.register(Book)
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display=('title', 'author', 'disply_genre')
+    def disply_genre(self,obj):
+        return ','.join(genre.name for genre in obj.genre.all()[:3])
+        disply_genre=short.description = 'genre'
+# admin.site.register(Author)
+@admin.register(Author)
+class AuthorAdmin(admin.ModelAdmin):
+    list_display=('last_name', 'first_name', 'date_of_birth', 'date_of_death')
+    fields=['last_name', 'first_name', ('date_of_birth', 'date_of_death')]
+admin.site.register(Genre)
+# admin.site.register(BookInstance)
+@admin.register(BookInstance)
+class BookInstanceAdmin(admin.ModelAdmin):
+    list_display=('book', 'status')
+admin.site.register(Language)
+
+
--- catalog/models.py
+++ catalog/models.py
@@ -29,6 +29,35 @@ class Genre(models.Model):
                 violation_error_message="Genre already exists (case insensitive match)"
             ),
         ]
+class Author(models.Model):
+    """Model representing an author."""
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    date_of_birth = models.DateField(null=True, blank=True)
+    date_of_death = models.DateField('Died', null=True, blank=True)
+
+    class Meta:
+        ordering = ['last_name', 'first_name']
+
+    def get_absolute_url(self):
+        """Returns the URL to access a particular author instance."""
+        return reverse('author-detail', args=[str(self.id)])
+
+    def __str__(self):
+        """String for representing the Model object."""
+        return f'{self.last_name}, {self.first_name}'
+
+class Language(models.Model):
+    """Model representing a Language (e.g. English, French, Japanese, etc.)"""
+    name = models.CharField(
+        max_length=200,
+        help_text="Enter the book's natural language (e.g. English, French, Japanese etc.)"
+    )
+
+    def __str__(self):
+        """String for representing the Model object."""
+        return self.name
+
 class Book(models.Model):
     """Model representing a book (but not a specific copy of a book)."""
     title = models.CharField(max_length=200)
@@ -36,7 +65,6 @@ class Book(models.Model):
     # Foreign Key used because book can only have one author, but authors can have multiple books.
     # Author as a string rather than object because it hasn't been declared yet in file.
     language = models.ForeignKey('Language', on_delete=models.SET_NULL, null=True)
-
 
     summary = models.TextField(
         max_length=1000, help_text="Enter a brief description of the book")
@@ -94,13 +122,4 @@ class BookInstance(models.Model):
         """String for representing the Model object."""
         return f'{self.id} ({self.book.title})'
     
-class Language(models.Model):
-    """Model representing a Language (e.g. English, French, Japanese, etc.)"""
-    name = models.CharField(
-        max_length=200,
-        help_text="Enter the book's natural language (e.g. English, French, Japanese etc.)"
-    )
 
-    def __str__(self):
-        """String for representing the Model object."""
-        return self.name
