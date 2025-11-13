da0a95dc2b08488d214dd134432a007d800c378c
yoniroich <y7639878@gmail.com>
2025-11-13 21:42:47 +0200

    Add config to setting.py

--- locallibrary/settings.py
+++ locallibrary/settings.py
@@ -31,7 +31,9 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure-&psk#na5l=p3q8
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG =os.environ.get('DJANGO_DEBUG', '') != 'False'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['web-production-3640.up.railway.app', '127.0.0.1']
+CSRF_TRUSTED_ORIGINS = ['https://web-production-3640.up.railway.app']
+
 
 
 # Application definition
