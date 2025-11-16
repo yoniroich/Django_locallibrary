21e227161502487a9ffdb0334ff852153072ecf5
yoniroich <y7639878@gmail.com>
2025-11-16 09:32:44 +0200

    update settings

--- locallibrary/settings.py
+++ locallibrary/settings.py
@@ -31,8 +31,10 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure-&psk#na5l=p3q8
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG =os.environ.get('DJANGO_DEBUG', '') != 'False'
 
-ALLOWED_HOSTS = ['web-production-12cf3.up.railway.app', '127.0.0.1']
-CSRF_TRUSTED_ORIGINS = ['https://web-production-12cf3.up.railway.app']
+ALLOWED_HOSTS = ['.railway.app', '127.0.0.1']
+CSRF_TRUSTED_ORIGINS = ['https://*.railway.app']
+SECURE_SSL_REDIRECT = True
+
 
 
 
